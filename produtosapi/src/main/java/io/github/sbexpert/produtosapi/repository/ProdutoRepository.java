f35b3d004794649ed5ec838fd29c574a693c7ec2
JhoctanTeixeira <jhoctan.teixeira@gmail.com>
2025-11-21 20:53:59 -0300

    initial recycling java study

---
+++ produtosapi/src/main/java/io/github/sbexpert/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package io.github.sbexpert.produtosapi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.sbexpert.produtosapi.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByNome(String nome);
+}
